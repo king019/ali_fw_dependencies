22fa8260e07224dea74a5d3a3363d7de5f2a00fe
k <k@qq.com>
2019-07-29 10:30:15 +0800

    1

--- ali_fw_dependencies/fw_common/src/main/java/com/k/common/FWPathUtil.java
+++ ali_fw_dependencies/fw_common/src/main/java/com/k/common/FWPathUtil.java
@@ -2,16 +2,22 @@ package com.k.common;
 
 public class FWPathUtil {
 
-  public static String getResource() {
-    String res;
-    res = "src/main/resources/";
-    return res;
-  }
+    public static String getResource() {
+        String res;
+        res = "src/main/resources/";
+        return res;
+    }
 
-  public static String getResource(String path) {
-    String res;
-    res = getResource().concat(path);
-    return res;
-  }
+    public static String getResource(String path) {
+        String res;
+        res = getResource().concat(path);
+        return res;
+    }
+
+    public static String getTargetPath() {
+        final String res;
+        res = "target/";
+        return res;
+    }
 
 }
